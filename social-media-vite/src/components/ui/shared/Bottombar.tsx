b2c02582f8cf229af4fe31fa4ae118eb799659b3
Katsuking <legendsoccerforte5@gmail.com>
2023-11-05 17:48:47 +0900

    bottom bar

--- social-media-vite/src/components/ui/shared/Bottombar.tsx
+++ social-media-vite/src/components/ui/shared/Bottombar.tsx
@@ -1,7 +1,29 @@
 // rafce
 
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
+
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+            key={link.label}
+          >
+            <p className="">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
